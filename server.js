6306de6f7af17da391d6fcd567f9f5e8df1e9378
Glitch (demo-imported-project) <none>
2021-04-03 01:37:44 +0000

    🐓🎯 Updated with Glitch by reena

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const server = new Koa();
 const PORT = process.env.PORT || 80
 
 server.use(function* () {
-    this.body = '<div style="text-align:center"><br/><br/><h2>Welocme to AppMixo®...........<h2/>';
+    this.body = '<div style="text-align:center"><br/><br/><h2>Welocme to AppMixo®<h2/>';
 });
 
 server.listen(PORT, function () {
-    console.log('Server running on PORT:', PORT)
+    console.log('Server running on PORT', PORT)
 });
