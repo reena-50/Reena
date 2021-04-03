a2957c699ac3a3d51abcf82ad891727a49e8e1da
Glitch (curse-pointed-language) <none>
2021-04-03 01:37:42 +0000

    Exporting to reena-50/Reena ./.glitch-assets:28580735/359 ./server.js:28580735/5

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const server = new Koa();
 const PORT = process.env.PORT || 80
 
 server.use(function* () {
-    this.body = '<div style="text-align:center"><br/><br/><h2>Welocme to AppMixo®...<h2/>';
+    this.body = '<div style="text-align:center"><br/><br/><h2>Welocme to AppMixo®<h2/>';
 });
 
 server.listen(PORT, function () {
